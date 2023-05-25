0236286e6ce2b345643a92c92007cc2020ac73e5
goose-ws <goose@goose.ws>
2023-05-25 09:34:41 -0400

    Update update-plex-in-docker.bash

--- update-plex-in-docker.bash
+++ update-plex-in-docker.bash
@@ -11,6 +11,10 @@
 #############################
 ##        Changelog        ##
 #############################
+# 2023-05-25
+# Added functionality to self determine container IP address
+# Added config options for verbosity
+# Both initiated via PR from ndoty
 # 2023-03-16
 # Rewrite of old script, removal of old script, and initial commit of new script
 
@@ -26,7 +30,7 @@
 ### Begin source, please don't edit below here. ###
 ###################################################
 
-if [[ -z "${BASH_VERSINFO}" || -z "${BASH_VERSINFO[0]}" || "${BASH_VERSINFO[0]}" -lt "4" ]]; then
+if [[ -z "${BASH_VERSINFO[0]}" || -z "${BASH_VERSINFO[0]}" || "${BASH_VERSINFO[0]}" -lt "4" ]]; then
     echo "This script requires Bash version 4 or greater"
     exit 255
 fi
@@ -39,7 +43,7 @@ for i in "${depArr[@]}"; do
             depFail="1"
         fi
     else
-        if ! command -v ${i} > /dev/null 2>&1; then
+        if ! command -v "${i}" > /dev/null 2>&1; then
             echo "${i}\\tnot found"
             depFail="1"
         fi
@@ -83,37 +87,85 @@ esac
 echo "${$}" >> "${lockFile}"
 
 # Define some functions
-badExit () {
-echo "${2}"
-rm -f "${lockFile}"
+function printOutput {
+if [[ "${1}" -le "${outputVerbosity}" ]]; then
+    echo "${0##*/}   ::   $(date "+%Y-%m-%d %H:%M:%S")   ::   [${1}] ${2}"
+fi
+}
+
+function removeLock {
+if rm -f "${lockFile}"; then
+    printOutput "3" "Lockfile removed"
+else
+    printOutput "1" "Unable to remove lockfile"
+fi
+}
+
+function badExit {
+printOutput "1" "${2}"
+removeLock
 exit "${1}"
 }
 
-cleanExit () {
-rm -f "${lockFile}"
+function cleanExit {
+removeLock
 exit 0
 }
 
-sendTelegramMessage () {
-eventText="<b>Episode Downloaded</b>$(printf "\r\n\r\n")${@}"
-telegramOutput="$(curl -skL --data-urlencode "text=${eventText}" "https://api.telegram.org/bot${telegramBotId}/sendMessage?chat_id=${telegramChannelId}&parse_mode=html" 2>&1)"
-if [[ "${?}" -ne "0" ]]; then
-    echo "API call to Telegram failed"
+function sendTelegramMessage {
+# Let's check to make sure our messaging credentials are valid
+telegramOutput="$(curl -skL "https://api.telegram.org/bot${telegramBotId}/getMe" 2>&1)"
+curlExitCode="${?}"
+if [[ "${curlExitCode}" -ne "0" ]]; then
+    badExit "1" "Curl to Telegram to check Bot ID returned a non-zero exit code: ${curlExitCode}"
+elif [[ -z "${telegramOutput}" ]]; then
+    badExit "2" "Curl to Telegram to check Bot ID returned an empty string"
 else
+    printOutput "3" "Curl exit code and null output checks passed"
+fi
+if ! [[ "$(jq ".ok" <<<"${telegramOutput,,}")" == "true" ]]; then
+    badExit "3" "Telegram bot API check failed"
+else
+    printOutput "2" "Telegram bot API key authenticated"
+    telegramOutput="$(curl -skL "https://api.telegram.org/bot${telegramBotId}/getChat?chat_id=${telegramChannelId}")"
+    curlExitCode="${?}"
+    if [[ "${curlExitCode}" -ne "0" ]]; then
+        badExit "4" "Curl to Telegram to check channel returned a non-zero exit code: ${curlExitCode}"
+    elif [[ -z "${telegramOutput}" ]]; then
+        badExit "5" "Curl to Telegram to check channel returned an empty string"
+    else
+        printOutput "3" "Curl exit code and null output checks passed"
+    fi
+    if ! [[ "$(jq ".ok" <<<"${telegramOutput,,}")" == "true" ]]; then
+        badExit "6" "Telegram channel check failed"
+    else
+        printOutput "2" "Telegram channel authenticated"
+    fi
+fi
+eventText="$(printf "<b>Plex Update for ${dockerHost%%.*}</b>\r\n\r\nPlex Media Server restarted for update from version <i>${myVer}</i> to version <i>${currVer}</i>")"
+telegramOutput="$(curl -skL --data-urlencode "text=${eventText}" "https://api.telegram.org/bot${telegramBotId}/sendMessage?chat_id=${telegramChannelId}&parse_mode=html" 2>&1)"
+curlExitCode="${?}"
+if [[ "${curlExitCode}" -ne "0" ]]; then
+    badExit "7" "Curl to Telegram returned a non-zero exit code: ${curlExitCode}"
+else
+    printOutput "3" "Curl returned zero exit code"
     # Check to make sure Telegram returned a true value for ok
     if ! [[ "$(jq ".ok" <<<"${telegramOutput}")" == "true" ]]; then
-        echo "Failed to send Telegram message:"
-        echo ""
-        echo "${telegramOutput}" | jq
-        echo ""
+        printOutput "1" "Failed to send Telegram message:"
+        printOutput "1" ""
+        printOutput "1" "$(jq <<<"${telegramOutput}")"
+        printOutput "1" ""
+    else
+        printOutput "2" "Telegram message sent successfully"
     fi
 fi
 }
 
-getNowPlaying () {
+function getNowPlaying {
 nowPlaying="$(curl -skL -m 15 "${plexAdd}/status/sessions?X-Plex-Token=${plexAccessToken}" | grep -Eo "size=\"[[:digit:]]+\"")"
 nowPlaying="${nowPlaying#*size=\"}"
 nowPlaying="${nowPlaying%%\"*}"
+printOutput "3" "Now playing count: ${nowPlaying}"
 }
 
 # Get config options
@@ -122,21 +174,26 @@ if [[ -z "${plexAccessToken}" ]]; then
     echo "Please specify a 'plexAccessToken=\"\"'"
     varFail="1"
 fi
-if [[ -z "${plexContainerName}" ]]; then
-    echo "Please specify a 'plexContainerName=\"\"'"
+if [[ -z "${plexPort}" ]]; then
+    echo "Please specify a 'plexPort=\"\"'"
+    varFail="1"
+elif ! [[ "${plexPort}" =~ ^[0-9]+$ ]]; then
+    echo "Please specify a numerical 'plexPort=\"\"'"
+    varFail="1"
+elif ! [[ "${plexPort}" -ge "0" && "${plexPort}" -le "65535" ]]; then
+    echo "Please specify a valid 'plexPort=\"\"'"
     varFail="1"
 fi
-if [[ -z "${plexIp}" ]]; then
-    if [[ ${verboseLogging} == "True" ]]; then echo "plexIp ENV not supplied, will attempt retrieving from Docker Container ${plexContainerName}"; fi
-    getIpFromDocker="True"
-elif ! [[ "${plexIp}" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.([0-9]{1,3}|[0-9]/[0-9]{1,2})$ ]]; then
-    echo "Please specify a valid plexIp x.x.x.x, or leave blank 'plexIP=\"${plexIp}\"'"
+if [[ -z "${plexScheme}" ]]; then
+    echo "Please specify a 'plexScheme=\"\"'"
     varFail="1"
-elif [[ $(ping -c 1 ${plexIp}; echo $?) == 1 ]]; then
-    echo "${plexIp} is unreachable, please specify a reachable Plex IP"
+elif ! [[ "${plexScheme}" =~ ^https?$ ]]; then
+    echo "Please specify a valid 'plexScheme=\"\"'"
     varFail="1"
-else
-    if [[ ${verboseLogging} == "True" ]]; then echo "${plexIp} is reachable on your network"; fi
+fi
+if [[ -z "${containerName}" ]]; then
+    echo "Please specify a 'containerName=\"\"'"
+    varFail="1"
 fi
 if [[ -z "${plexVersion}" ]]; then
     echo "Please specify a 'plexVersion=\"\"'"
@@ -147,7 +204,6 @@ else
         varFail="1"
     fi
 fi
-
 # TODO: Automate this variable via 'uname'
 if [[ -z "${hostOS}" ]]; then
     echo "Please specify a 'hostOS=\"\"'"
@@ -174,84 +230,104 @@ if [[ -z "${hostCodecPath}" ]] || ! [[ -d "${hostCodecPath%/}" ]]; then
     echo "Please specify a 'hostCodecPath=\"\"'"
     varFail="1"
 fi
-if [[ "${varFail}" -eq "1" ]]; then
-    badExit "1" "Please fix above errors";
-fi
-
-### Source
-# Can we check for updates?
 if ! [[ "${updateCheck,,}" =~ ^(yes|no|true|false)$ ]]; then
     echo "Option to check for updates not valid. Assuming no."
     updateCheck="No"
 fi
+if ! [[ "${outputVerbosity}" =~ ^[1-3]$ ]]; then
+    echo "Invalid output verbosity defined. Assuming level 1 (Errors only)"
+    outputVerbosity="1"
+fi
+if [[ "${varFail}" -eq "1" ]]; then
+    badExit "8" "Please fix above errors"
+fi
+
+### Source
+# Can we check for updates?
 if [[ "${updateCheck,,}" =~ ^(yes|true)$ ]]; then
     newest="$(curl -skL "https://raw.githubusercontent.com/goose-ws/bash-scripts/main/update-plex-in-docker.bash" | md5sum | awk '{print $1}')"
     current="$(md5sum "${0}" | awk '{print $1}')"
     if ! [[ "${newest}" == "${current}" ]]; then
-        echo "A newer version is available"
+        # Although it's not an error, we should always be allowed to print this message if update checks are allowed, so giving it priority 1
+        printOutput "1" "A newer version is available"
+    else
+        printOutput "2" "No new updates available"
     fi
 fi
 
 ### Build variable ${plexServerAddress} here
 # Get the IP address of the Plex container
-if [[ "${getIpFromDocker}" == "True" ]]; then
-	dockerIp="$(docker inspect "${plexContainerName}" | jq ".[].HostConfig.NetworkMode")"
-	dockerIp="${dockerIp#\"}"
-	dockerIp="${dockerIp%\"}"
-	if [[ "${dockerIp%%:*}" == "container" ]]; then
-                # Networking is being run through another container. So we need that container's IP address.
-		dockerIp="$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${dockerIp#container:}")"
-	else
-		unset dockerIp
-		dockerIp="$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${plexContainerName}")"
-	fi
-        if ! [[ "${dockerIp}" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.([0-9]{1,3}|[0-9]/[0-9]{1,2})$ ]]; then
-            echo "Got Bad Plex IP Docker inspect: ${dockerIp}"
-            badExit "2" "Unable to determine PMS IP address, try specifying directly in .env with plexIp=\"x.x.x.x.\"";
+if [[ -z "${containerIp}" ]]; then
+    printOutput "2" "Attempting to automatically determine container IP address"
+    # Find the type of networking the container is using
+    containerNetworking="$(docker container inspect --format '{{range $net,$v := .NetworkSettings.Networks}}{{printf "%s" $net}}{{end}}' "${containerName}")"
+    printOutput "3" "Networking type: ${containerNetworking}"
+    if [[ -z "${containerNetworking}" ]]; then
+        printOutput "2" "No network type defined. Checking to see if networking is through another container."
+        # IP address returned blank. Is it being networked through another container?
+        containerIp="$(docker inspect "${containerName}" | jq ".[].HostConfig.NetworkMode")"
+        containerIp="${containerIp#\"}"
+        containerIp="${containerIp%\"}"
+        printOutput "3" "Network mode: ${containerIp%%:*}"
+        if [[ "${containerIp%%:*}" == "container" ]]; then
+            # Networking is being run through another container. So we need that container's IP address.
+            printOutput "2" "Networking routed through another container. Retrieving IP address."
+            containerIp="$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${containerIp#container:}")"
         else
-	    if [[ ${verboseLogging} == "True" ]]; then echo "Successfully grabed the IP ${dockerIp} from Docker"; fi
-            plexIp=dockerIp
+            printOutput "1" "Unable to determine networking type"
+            unset containerIp
         fi
+    elif [[ "${containerNetworking}" == "host" ]]; then
+        # Host networking, so we can probably use localhost
+        containerIp="127.0.0.1"
+    else
+        # Something else. Let's see if we can get it via inspect.
+        containerIp="$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "${containerName}")"
+    fi
+    if [[ -z "${containerIp}" ]] || ! [[ "${containerIp}" =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.([0-9]{1,3}|[0-9]/[0-9]{1,2})$ ]]; then
+        badExit "9" "Unable to determine IP address"
+    else
+        printOutput "2" "Container IP address: ${containerIp}"
+    fi
 fi
-
-# We can assume the scheme is http and port is 32400 unless someone reports otherwise
-plexScheme="${plexScheme:=http}"
-plexPort="${plexPort:=32400}"
 
 # Build our address
-plexAdd="${plexScheme}://${plexIp}:${plexPort}"
+plexAdd="${plexScheme}://${containerIp}:${plexPort}"
+printOutput "3" "Server address: ${plexAdd}"
 
-# Make sure we can check our version
+# Make sure our server is reachable, and we can check our version
 myVer="$(curl -skL -m 15 "${plexAdd}/servers?X-Plex-Token=${plexAccessToken}")"
-if [[ "${?}" -ne "0" ]]; then
-    badExit "3" "Unable to check local version";
+curlExitCode="${?}"
+if [[ "${curlExitCode}" -ne "0" ]]; then
+    badExit "10" "Unable to check local version, curl returned non-zero exit code: ${curlExitCode}"
 fi
-
-# Make sure we can check the latest version
-currVer="$(curl -skL -m 15 "https://plex.tv/api/downloads/1.json?channel=${plexVersion}")"
-if [[ "${?}" -ne "0" ]]; then
-    badExit "4" "Unable to check latest version";
-fi
-
 myVer="$(grep -Ev "^<\?xml" <<<"${myVer}" | grep -Eo "version=\"([[:alnum:]]|\.|-)+\"")"
 myVer="${myVer#*version=\"}"
 myVer="${myVer%%\"*}"
 if [[ "${myVer}" == "null" ]] || [[ -z "${myVer}" ]]; then
-    badExit "5" "Unable to parse local version";
+    badExit "11" "Unable to parse local version"
+else
+    printOutput "2" "Detected local version: ${myVer}"
 fi
 
+# Make sure we can check the latest version
+currVer="$(curl -skL -m 15 "https://plex.tv/api/downloads/1.json?channel=${plexVersion}")"
+curlExitCode="${?}"
+if [[ "${curlExitCode}" -ne "0" ]]; then
+    badExit "12" "Unable to check latest version, curl returned non-zero exit code: ${curlExitCode}"
+fi
 currVer="$(jq ".computer.${hostOS}.version" <<<"${currVer}")"
 currVer="${currVer#\"}"
 currVer="${currVer%\"}"
 if [[ "${currVer}" == "null" ]] || [[ -z "${currVer}" ]]; then
-    badExit "6" "Unable to parse latest version";
+    badExit "13" "Unable to parse latest version"
+else
+    printOutput "2" "Detected current version: ${currVer}"
 fi
 
-if [[ ${verboseLogging} == "True" ]]; then echo "My Version: ${myVer} <> Current Version: ${currVer}"; fi
-
 if [[ "${myVer}" == "${currVer}" ]]; then
-	if [[ ${verboseLogging} == "True" ]]; then echo "Versions match, no need for update"; fi
-        cleanExit;
+    printOutput "2" "Versions match, no update needed"
+    cleanExit;
 fi
 
 # If we've gotten this far, version strings do not match
@@ -260,7 +336,8 @@ myVer2="${myVer2//./}"
 currVer2="${currVer%-*}"
 currVer2="${currVer2//./}"
 if [[ "${myVer2}" -gt "${currVer2}" ]]; then
-    if [[ ${verboseLogging} == "True" ]]; then echo "We already have a version more recent (${myVer}) than the current (${currVer}), probably a beta/Plex Pass version"; fi
+    # We already have a version more recent than the current, probably a beta/Plex Pass version
+    printOutput "2" "Local version newer than current stable version."
     cleanExit;
 fi
 
@@ -268,69 +345,53 @@ fi
 getNowPlaying;
 
 if [[ "${nowPlaying}" -ne "0" ]]; then
-    if [[ ${verboseLogging} == "True" ]]; then echo "At least one person is watching something"; fi
-    if [[ ${verboseLogging} == "True" ]]; then echo "We'll try again at the next cron run"; fi
-	cleanExit;
+    # At least one person is watching something
+    # We'll try again at the next cron run
+    printOutput "2" "Detected ${nowPlaying} users currently using Plex"
+    cleanExit;
 fi
 
 # Nobody is watching anything. Maybe someone was between episodes? Let's wait 1 minute and check.
+printOutput "3" "Sleeping for 60 seconds before re-checking play status"
 sleep 60
 
 # Is anyone playing anything?
 getNowPlaying;
 
 if [[ "${nowPlaying}" -ne "0" ]]; then
-    if [[ ${verboseLogging} == "True" ]]; then echo "At least one person is still watching something"; fi
-    if [[ ${verboseLogging} == "True" ]]; then echo "We'll try again at the next cron run"; fi
-	cleanExit;
+    # At least one person is watching something
+    # We'll try again at the next cron run
+    printOutput "2" "Detected ${nowPlaying} users currently using Plex"
+    cleanExit;
 fi
 
-if [[ ${verboseLogging} == "True" ]]; then echo "Nice, nobody's watching anything. Let's restart the Docker container."; fi
+# Nice, nobody's watching anything. Let's restart the Docker container.
 # Get the Docker container ID
-docker stop "${plexContainerName}"
+printOutput "2" "Stopping container"
+if docker stop "${containerName}"; then
+    printOutput "3" "Container stopped successfully"
+else
+    badExit "14" "Unable to stop container"
+fi
 
 # Clean out the Codecs folder, because apparently that sometimes breaks things between upgrades if you don't
 # https://old.reddit.com/r/PleX/comments/lzwkyc/eae_timeout/gq4xcat/
-rm -rf "${hostCodecPath%/}"/*
-
-docker start "${plexContainerName}"
-dockerHost="$(</etc/hostname)"
-if [[ -n "${telegramBotId}" && -n "${telegramChannelId}" ]]; then
-	# Let's check to make sure our messaging credentials are valid
-    telegramOutput="$(curl -skL "https://api.telegram.org/bot${telegramBotId}/getMe" 2>&1)"
-    curlExitCode="${?}"
-    if [[ "${curlExitCode}" -ne "0" ]]; then
-        badExit "7" "Curl to Telegram to check Bot ID returned a non-zero exit code";
-    elif [[ -z "${telegramOutput}" ]]; then
-        badExit "8" "Curl to Telegram to check Bot ID returned an empty string";
-    fi
-    if ! [[ "$(jq ".ok" <<<"${telegramOutput,,}")" == "true" ]]; then
-        badExit "9" "Telegram bot API check failed";
-    else
-        telegramOutput="$(curl -skL "https://api.telegram.org/bot${telegramBotId}/getChat?chat_id=${telegramChannelId}")"
-        curlExitCode="${?}"
-        if [[ "${curlExitCode}" -ne "0" ]]; then
-            badExit "10" "Curl to Telegram to check channel returned a non-zero exit code";
-        elif [[ -z "${telegramOutput}" ]]; then
-            badExit "11" "Curl to Telegram to check channel returned an empty string";
-        fi
-        if ! [[ "$(jq ".ok" <<<"${telegramOutput,,}")" == "true" ]]; then
-            badExit "12" "Telegram channel API check failed";
-        fi
-    fi
-    eventText="$(printf "<b>Plex Update for ${dockerHost%%.*}</b>\r\n\r\nPlex Media Server restarted for update from version <i>${myVer}</i> to version <i>${currVer}</i>")"
-	telegramOutput="$(curl -skL --data-urlencode "text=${eventText}" "https://api.telegram.org/bot${telegramBotId}/sendMessage?chat_id=${telegramChannelId}&parse_mode=html" 2>&1)"
-	if [[ "${?}" -ne "0" ]]; then
-		echo "API call to Telegram failed"
-	else
-		# Check to make sure Telegram returned a true value for ok
-		if ! [[ "$(jq ".ok" <<<"${telegramOutput}")" == "true" ]]; then
-			echo "Failed to send Telegram message:"
-			echo ""
-			echo "${telegramOutput}" | jq
-			echo ""
-		fi
-	fi
+printOutput "2" "Cleaning out Codecs directory"
+if rm -rf "${hostCodecPath%/}"/*; then
+    printOutput "3" "Codecs directory cleared successfully"
+else
+    badExit "15" "Unable to clear Codecs directory"
 fi
 
-cleanExit;
+printOutput "2" "Starting container"
+if docker start "${containerName}"; then
+    printOutput "3" "Container started successfully"
+else
+    badExit "16" "Unable to start container"
+fi
+dockerHost="$(</etc/hostname)"
+printOutput "3" "Got hostname: ${dockerHost}"
+if [[ -n "${telegramBotId}" && -n "${telegramChannelId}" ]]; then
+    printOutput "2" "Telegram messaging enabled -- Checking credentials"
+    sendTelegramMessage
+fi
